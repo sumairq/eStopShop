b030f6f5642e056164e1622356b16b9702d00a6a
sumairq <sumairqaisar992@gmail.com>
2023-11-12 23:29:51 +0500

    Add Watchers in app.vue

--- app.vue
+++ app.vue
@@ -18,4 +18,25 @@
 <script setup>
 import { useUserStore } from "~/stores/user";
 const userStore = useUserStore();
+
+const route = useRoute()
+
+let windowWidth = ref(process.client ? window.innerWidth : '')
+
+onMounted(() => {
+    userStore.isLoading = true
+    window.addEventListener('resize', function () {
+        windowWidth.value = window.innerWidth;
+    });
+})
+
+watch(() => windowWidth.value, () => {
+    if (windowWidth.value >= 767) {
+        userStore.isMenuOverlay = false
+    }
+})
+
+watch(() => route.fullPath, () => {
+    userStore.isLoading = true
+})
 </script>

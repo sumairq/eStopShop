24ef04d4ed1d71a36a1a4739dc1f7f5a5d1a2cf7
sumairq <sumairqaisar992@gmail.com>
2023-11-12 22:07:38 +0500

    Initialize project with navigation layout

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template lang="">
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+    IndexPage
+    </div>
+  </MainLayout>
+</template>
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+</script>

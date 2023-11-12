f94fe89da095eacd55a2515315007f950bd7ee23
sumairq <sumairqaisar992@gmail.com>
2023-11-12 23:16:28 +0500

    Add textinput component

---
+++ pages/auth.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="AuthPage" class="w-full h-[100vh] bg-white">
+        <div class="w-full flex items-center justify-center p-5 border-b border-b-gray-300">
+            <NuxtLink to="/" class="min-w-[170px]">
+                <img width="170" src="/AliExpress-logo.png">
+            </NuxtLink>
+        </div>
+
+    
+</template>
+
+<script setup>
+const client = useSupabaseClient()
+const user = useSupabaseUser()
+
+watchEffect(() => {
+    if (user.value) {
+        return navigateTo('/')
+    }
+})
+
+const login = async (prov) => {
+  const { data, error } = await client.auth.signInWithOAuth({
+    provider: prov,
+    redirectTo: window.location.origin
+  })
+}
+</script>

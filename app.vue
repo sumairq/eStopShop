81226e9c02f02cb65ec85054defaaf10574ce7be
sumairq <sumairqaisar992@gmail.com>
2023-11-12 23:26:54 +0500

    Use menu overlay in app

--- app.vue
+++ app.vue
@@ -1,4 +1,21 @@
 <template>
-  <div class="fixed z-[-1] bg-[#F2F2F2] w-full h-[100vh]"/> 
-  <NuxtPage/>
+  <div class="fixed z-[-1] bg-[#F2F2F2] w-full h-[100vh]" />
+  <NuxtPage />
+  <MenuOverlay
+    :class="[
+      {
+        'max-h-[100vh] transition-all duration-200 ease-in visible':
+          userStore.isMenuOverlay,
+      },
+      {
+        'max-h-0 transition-all duration-200 ease-out invisible':
+          !userStore.isMenuOverlay,
+      },
+    ]"
+  />
 </template>
+
+<script setup>
+import { useUserStore } from "~/stores/user";
+const userStore = useUserStore();
+</script>

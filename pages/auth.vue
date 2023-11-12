01f2d7750e20fce2ea6a06d6389d698699af6e5c
sumairq <sumairqaisar992@gmail.com>
2023-11-12 23:19:38 +0500

    Add login/register section

--- pages/auth.vue
+++ pages/auth.vue
@@ -6,7 +6,53 @@
             </NuxtLink>
         </div>
 
-    
+        <div class="max-w-[400px] mx-auto px-2">
+
+            <div class="text-center my-6">Login / Register</div>
+
+            <button 
+                @click="login('google')"
+                class="
+                    flex 
+                    items-center 
+                    justify-center 
+                    gap-3
+                    p-1.5
+                    w-full 
+                    border 
+                    hover:bg-gray-100
+                    rounded-full
+                    text-lg
+                    font-semibold
+                "
+            >
+                <img class="w-full max-w-[30px]" src="/google-logo.png">
+                <div>Google</div>
+            </button>
+
+            <button 
+                @click="login('github')"
+                class="
+                mt-4
+                    flex 
+                    items-center 
+                    justify-center 
+                    gap-3
+                    p-1.5
+                    w-full 
+                    border 
+                    hover:bg-gray-100
+                    rounded-full
+                    text-lg
+                    font-semibold
+                "
+            >
+                <img class="w-full max-w-[30px]" src="/github-logo.png">
+                <div>Github</div>
+            </button>
+
+        </div>
+    </div>
 </template>
 
 <script setup>
